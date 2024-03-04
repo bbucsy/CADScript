2caf421dfabba32d0629db21cb0ce21bd3595699
Benjamin Bucsy <benjamin.bucsy@benestudio.co>
2024-03-04 18:19:24 +0100

    web wip

--- project/packages/cad-script/tsup.config.ts
+++ project/packages/cad-script/tsup.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-	entry: ['src/language/lib.ts'],
+	entry: ['src/language/lib.ts', 'src/language/main-browser.ts'],
 	outDir: './lib',
 	splitting: false,
 	sourcemap: true,
 	clean: true,
 	dts: true,
 	treeshake: true,
+	minify: true,
 	format: 'esm'
 })
